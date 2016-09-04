8308321249d634ecd6ea6ecc38a0a4bc5c370e3f
John <jiao.xuejiao@gmail.com>
2016-09-05 00:16:30 +0800

    serve doge image in server

--- 1server.py
+++ 1server.py
@@ -9,14 +9,38 @@ addr = (host, port)
 server.bind(addr)
 print 'server at {}:{}'.format(host, port)
 
+def from_file(path):
+    filename = path.split('/')[-1]
+    print 'fielname'
+    print filename
+    response = b'''HTTP/1.1 200 OK
+Content-Type: image/gif
+
+'''
+    with open(filename, 'rb') as f:
+        content = f.read()
+        response += content
+        return response
+
 while True:
     server.listen(5)
     client, address = server.accept()
-    print 'client address'
-    print address
     request = client.recv(2048)
-    print 'request'
-    print request.decode('utf-8')
-    response = 'hello world'.encode('utf-8')
+    request = request.decode('utf-8')
+    line = request.split('\n')[0]
+    path = line.split()[1]
+    print 'path'
+    print path
+    if path in ['/doge1.gif', '/doge2.gif']:
+        response = from_file(path)
+    else:
+        response = b'''HTTP/1.1 200 OK
+Content-Type: text/html
+
+<h1>Hello World!</h1>
+<img src="/doge1.gif">
+<img src="/doge2.gif">'''
     client.send(response)
     client.close()
+
+
