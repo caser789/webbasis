e5499055a369f15cda469f525af072eb0b49aaee
John <jiao.xuejiao@gmail.com>
2016-09-04 23:33:07 +0800

    use client to get a web page

--- 1client.py
+++ 1client.py
@@ -3,7 +3,7 @@ import socket
 # s = socket.socket(socket.AF_INET, socket.sock_STREAM)
 s = socket.socket()
 
-host = 'g.cn'
+host = 'www.thebeijinger.com'
 port = 80
 addr = (host, port)
 s.connect(addr)
@@ -11,9 +11,20 @@ print 'get socket name'
 print s.getsockname()
 http_request = """GET / HTTP/1.1
 host: {}
+Connection: close
 
 """.format(host)
 s.send(http_request.encode('utf-8'))
-http_response = s.recv(1023)
+response = b''
+while True:
+    r = s.recv(1024)
+    if len(r) == 0:
+        break
+    response += r
+    print 'chunk'
+
 print '*' * 40
-print http_response.decode('utf-8')
+print response.decode('utf-8')
+with open('{}.html'.format(host), 'wb') as f:
+    f.write(response)
+print '*' * 40
